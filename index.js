9211e4e0ad921c2c2409dbf977bbffc680f11025
Denis Malinochkin <dmalinochkin@rambler.ru>
2016-06-09 15:11:46 +0300

    ES2015

--- index.js
+++ index.js
@@ -1,20 +1,41 @@
 'use strict';
 
-var fs = require('fs');
-var through = require('through2');
-var chalk = require('chalk');
-var Promise = require('promise');
-var statP = Promise.denodeify(fs.stat);
-var rimrafP = Promise.denodeify(require('rimraf'));
-var objectAssign = require('object-assign');
-var globby = require('globby');
-var globParent = require('glob-parent');
-var cpf = require('cp-file');
+const fs = require('fs');
+const through = require('through2');
+const chalk = require('chalk');
+const rimraf = require('rimraf');
+const globby = require('globby');
+const globParent = require('glob-parent');
+const cpf = require('cp-file');
 
-var files = require('./lib/files');
+const files = require('./lib/files');
 
-var plugin = function(src, dest, options) {
-  var opts = objectAssign({
+function statP(filepath) {
+  return new Promise((resolve, reject) => {
+    fs.stat(filepath, (err, stats) => {
+      if (err) {
+        reject(err);
+      }
+
+      resolve(stats);
+    });
+  });
+}
+
+function rimrafP(filepath, options) {
+  return new Promise((resolve, reject) => {
+    rimraf(filepath, options, (err) => {
+      if (err) {
+        reject(err);
+      }
+
+      resolve();
+    });
+  });
+}
+
+function m(src, dest, options) {
+  const opts = Object.assign({
     updateAndDelete: true,
     verbose: false,
     ignoreInDest: []
@@ -24,14 +45,13 @@ var plugin = function(src, dest, options) {
     this.push(file);
     cb();
   }, function(cb) {
-    var _that = this;
     if (!src || !dest) {
       this.emit('error', 'Give the source directory and destination directory');
       return cb();
     }
 
     // If `verbose` mode is enabled
-    var log = (opts.verbose) ? console.log : function() {};
+    const log = (opts.verbose) ? console.log : function() {};
 
     // Remove latest slash for base path
     if (opts.base && opts.base.slice(-1) === '/') {
@@ -44,7 +64,7 @@ var plugin = function(src, dest, options) {
     }
 
     // Settings for globby
-    var globPromises = [
+    const globPromises = [
       globby(src, { dot: true, nosort: true }),
       globby('**', {
         cwd: dest,
@@ -55,22 +75,22 @@ var plugin = function(src, dest, options) {
     ];
 
     Promise.all(globPromises)
-      .then(function(results) {
+      .then((results) => {
         // Deleting files
         if (opts.updateAndDelete) {
           // Create a full list of the basic directories
-          var basePaths = [''];
-          src.forEach(function(srcGlob) {
-            var baseDir = files.expandDirTree(globParent(srcGlob));
+          let basePaths = [''];
+          src.forEach((srcGlob) => {
+            const baseDir = files.expandDirTree(globParent(srcGlob));
             basePaths = basePaths.concat(baseDir);
           });
 
           // Search unique files to the destination directory
           // To files in the source directory are added paths to basic directories
-          var fullSourcePaths = results[0].concat(basePaths);
-          results[1] = results[1].filter(function(filepath) {
-            var fullpath = files.fromDestToSrcPath(filepath, dest, opts);
-            for (var i = 0; i < fullSourcePaths.length; i++) {
+          const fullSourcePaths = results[0].concat(basePaths);
+          results[1] = results[1].filter((filepath) => {
+            const fullpath = files.fromDestToSrcPath(filepath, dest, opts);
+            for (let i = 0; i < fullSourcePaths.length; i++) {
               if (fullSourcePaths[i].indexOf(fullpath) + 1) {
                 return false;
               }
@@ -80,23 +100,23 @@ var plugin = function(src, dest, options) {
           });
 
           // Creating promises to delete files
-          results[1] = results[1].map(function(destPath) {
-            var fullpath = files.fromSrcToDestPath(destPath, dest, {});
+          results[1] = results[1].map((destPath) => {
+            const fullpath = files.fromSrcToDestPath(destPath, dest, {});
             return rimrafP(fullpath, { glob: false })
               .then(log(chalk.red('Removing: ') + fullpath))
-              .catch(function(err) {
-                _that.emit('error', 'Cannot remove `' + fullpath + '`: ' + err.code);
+              .catch((err) => {
+                this.emit('error', `Cannot remove '${fullpath}': ${err.code}`);
                 return cb();
               });
           });
         }
 
         // Copying files
-        results[0] = results[0].map(function(srcPath) {
-          var to = files.fromSrcToDestPath(srcPath, dest, opts);
-          var statSrc = statP(srcPath);
-          var statDest = statP(to).catch(function() {});
-          return Promise.all([statSrc, statDest]).then(function(stats) {
+        results[0] = results[0].map((srcPath) => {
+          const to = files.fromSrcToDestPath(srcPath, dest, opts);
+          const statSrc = statP(srcPath);
+          const statDest = statP(to).catch(() => {});
+          return Promise.all([statSrc, statDest]).then((stats) => {
             // Update file?
             if ((typeof stats[1] !== 'undefined' && !opts.updateAndDelete) ||
               stats[0].isDirectory() || stats[1] && files.compareTime(stats[0], stats[1])) {
@@ -106,27 +126,25 @@ var plugin = function(src, dest, options) {
             return cpf(srcPath, to)
               // Display log messages when copying files
               .then(log(chalk.green('Copying: ') + srcPath + chalk.cyan(' -> ') + to))
-              .catch(function(err) {
-                _that.emit('error', 'Cannot copy from `' + srcPath + '` to `' + to + '`: ' + err.message);
+              .catch((err) => {
+                this.emit('error', `'${srcPath}'' to '${to}': ${err.message}`);
                 return cb();
               });
           });
         });
 
         // Flatten nested array.
-        return Promise.all(results.reduce(function(a, b) {
-          return a.concat(b);
-        }));
+        return Promise.all(results.reduce((a, b) => a.concat(b)));
       })
-      .then(function() {
-        _that.resume();
+      .then(() => {
+        this.resume();
         cb();
       })
-      .catch(function(err) {
-        _that.emit('error', err);
+      .catch((err) => {
+        this.emit('error', err);
         return cb();
       });
   });
-};
+}
 
-module.exports = plugin;
+module.exports = m;
--- test/test.js
+++ test/test.js
@@ -1,25 +1,25 @@
 'use strict';
 
-var path = require('path');
-var fs = require('fs');
-var assert = require('assert');
-var mkdir = require('mkdirp');
+const path = require('path');
+const fs = require('fs');
+const assert = require('assert');
+const mkdir = require('mkdirp');
 
-var syncy = require('..');
-var existsSync = require('../lib/files').existsStatSync;
+const syncy = require('..');
+const existsSync = require('../lib/files').existsStatSync;
 
 // Recursively read a directory
-var recurseReadDir = function(root, files, prefix) {
+function recurseReadDir(root, files, prefix) {
   prefix = prefix || '';
   files = files || [];
 
-  var dir = path.join(root, prefix);
+  const dir = path.join(root, prefix);
   if (!existsSync(dir)) {
     return files;
   }
 
   if (fs.statSync(dir).isDirectory()) {
-    fs.readdirSync(dir).forEach(function(name) {
+    fs.readdirSync(dir).forEach((name) => {
       recurseReadDir(root, files, path.join(prefix, name));
     });
   } else {
@@ -27,191 +27,240 @@ var recurseReadDir = function(root, files, prefix) {
   }
 
   return files;
-};
+}
 
 // Creating test files
-var createFiles = function(filepath, count) {
+function createFiles(filepath, count) {
   mkdir.sync(filepath);
-  for (var i = 0; i < count; i++) {
-    var testFile = path.join(filepath, 'test-' + i + '.txt');
+  for (let i = 0; i < count; i++) {
+    const testFile = path.join(filepath, 'test-' + i + '.txt');
     fs.writeFileSync(testFile, 'test');
   }
-};
+}
 
 // Look ma, it's cp -R.
-var copyRecursiveSync = function(src, dest) {
-  var exists = existsSync(src);
-  var stats = exists && fs.statSync(src);
-  var isDirectory = exists && stats.isDirectory();
+function copyRecursiveSync(src, dest) {
+  const exists = existsSync(src);
+  const stats = exists && fs.statSync(src);
+  const isDirectory = exists && stats.isDirectory();
+
   if (exists && isDirectory) {
     mkdir.sync(dest);
-    fs.readdirSync(src).forEach(function(childItemName) {
+    fs.readdirSync(src).forEach((childItemName) => {
       copyRecursiveSync(path.join(src, childItemName), path.join(dest, childItemName));
     });
-  } else {
-    fs.writeFileSync(dest, fs.readFileSync(src));
+
+    return;
   }
-};
 
-describe('Default tasks', function() {
-  it('Copying', function(done) {
-    syncy(['test/fixtures/**/*'], '.tmp/glob')
-      .on('end', function() {
-        var result = recurseReadDir('.tmp/glob');
-        assert.equal(result.length, 8);
-        done();
-      })
-      .end();
+  fs.writeFileSync(dest, fs.readFileSync(src));
+}
+
+describe('Default tasks', () => {
+  it('Copying', (done) => {
+    const stream = syncy(['test/fixtures/**/*'], '.tmp/glob');
+
+    stream.on('end', () => {
+      const result = recurseReadDir('.tmp/glob');
+      assert.equal(result.length, 8);
+      done();
+    });
+
+    stream.end();
   });
 
-  it('Copying with base', function(done) {
-    syncy(['test/fixtures/**'], '.tmp/base', { base: 'test/fixtures' })
-      .on('end', function() {
-        var result = recurseReadDir('.tmp/base');
-        assert.equal(result.length, 8);
-        done();
-      })
-      .end();
+  it('Copying with base', (done) => {
+    const stream = syncy(['test/fixtures/**'], '.tmp/base', {
+      base: 'test/fixtures'
+    });
+
+    stream.on('end', () => {
+      const result = recurseReadDir('.tmp/base');
+      assert.equal(result.length, 8);
+      done();
+    });
+
+    stream.end();
   });
 
-  it('Removing files', function(done) {
+  it('Removing files', (done) => {
     createFiles('.tmp/remove/test/fixtures', 3);
-    syncy(['test/fixtures/**'], '.tmp/remove')
-      .on('end', function() {
-        var result = recurseReadDir('.tmp/remove');
-        assert.equal(result.length, 8);
-        done();
-      })
-      .end();
+    const stream = syncy(['test/fixtures/**'], '.tmp/remove');
+
+    stream.on('end', () => {
+      const result = recurseReadDir('.tmp/remove');
+      assert.equal(result.length, 8);
+      done();
+    });
+
+    stream.end();
   });
 
-  it('Skipping files', function(done) {
-    syncy(['test/fixtures/**'], '.tmp/skip')
-      .on('end', function() {
-        syncy(['test/fixtures/**'], '.tmp/skip')
-          .on('end', function() {
-            var result = recurseReadDir('.tmp/skip');
-            assert.equal(result.length, 8);
-            done();
-          })
-          .end();
-      })
-      .end();
+  it('Skipping files', (done) => {
+    const stream = syncy(['test/fixtures/**'], '.tmp/skip');
+
+    stream.on('end', () => {
+      syncy(['test/fixtures/**'], '.tmp/skip')
+        .on('end', () => {
+          const result = recurseReadDir('.tmp/skip');
+          assert.equal(result.length, 8);
+          done();
+        })
+        .end();
+    });
+
+    stream.end();
   });
 });
 
-describe('Updating files', function() {
-  it('Remove file in `dest`', function(done) {
-    syncy(['test/fixtures/**'], '.tmp/update_dest')
-      .on('end', function() {
-        // Remove one file in the destination directory
-        fs.unlinkSync('.tmp/update_dest/test/fixtures/folder-1/test.txt');
-        syncy(['test/fixtures/**'], '.tmp/update_dest')
-          .on('end', function() {
-            var result = recurseReadDir('.tmp/update_dest');
-            assert.equal(result.length, 8);
-            done();
-          })
-          .end();
-      })
-      .end();
+describe('Updating files', () => {
+  it('Remove file in `dest`', (done) => {
+    const streamOne = syncy(['test/fixtures/**'], '.tmp/update_dest');
+    const streamTwo = syncy(['test/fixtures/**'], '.tmp/update_dest');
+
+    streamOne.on('end', () => {
+      // Remove one file in the destination directory
+      fs.unlinkSync('.tmp/update_dest/test/fixtures/folder-1/test.txt');
+
+      streamTwo.on('end', () => {
+        const result = recurseReadDir('.tmp/update_dest');
+        assert.equal(result.length, 8);
+        done();
+      });
+
+      streamTwo.end();
+    });
+
+    streamOne.end();
   });
 
-  it('Remove file in `src`', function(done) {
+  it('Remove file in `src`', (done) => {
     // Backup test files
     copyRecursiveSync('test/fixtures', '.tmp/fixtures_backup');
-    syncy(['.tmp/fixtures_backup/**'], '.tmp/update_src')
-      .on('end', function() {
-        // Remove one file in the source directory
-        fs.unlinkSync('.tmp/fixtures_backup/folder-1/test.txt');
-        syncy(['.tmp/fixtures_backup/**'], '.tmp/update_src')
-          .on('end', function() {
-            var result = recurseReadDir('.tmp/update_src');
-            assert.equal(result.length, 7);
-            done();
-          })
-          .end();
-      })
-      .end();
+
+    const streamOne = syncy(['.tmp/fixtures_backup/**'], '.tmp/update_src');
+    const streamTwo = syncy(['.tmp/fixtures_backup/**'], '.tmp/update_src');
+
+    streamOne.on('end', () => {
+      // Remove one file in the source directory
+      fs.unlinkSync('.tmp/fixtures_backup/folder-1/test.txt');
+
+      streamTwo.on('end', () => {
+        const result = recurseReadDir('.tmp/update_src');
+        assert.equal(result.length, 7);
+        done();
+      });
+
+      streamTwo.end();
+    });
+
+    streamOne.end();
   });
 
-  it('Remove file in `src` (with `**/*` pattern)', function(done) {
+  it('Remove file in `src` (with `**/*` pattern)', (done) => {
     // Backup test files
     copyRecursiveSync('test/fixtures', '.tmp/fixtures_backup_issue_1');
-    syncy(['.tmp/fixtures_backup_issue_1/**/*.txt'], '.tmp/issue_1')
-      .on('end', function() {
-        // Remove one file in the source directory
-        fs.unlinkSync('.tmp/fixtures_backup_issue_1/folder-1/test.txt');
-        syncy(['.tmp/fixtures_backup_issue_1/**/*.txt'], '.tmp/issue_1')
-          .on('end', function() {
-            var result = recurseReadDir('.tmp/issue_1');
-            assert.equal(result.length, 7);
-            done();
-          })
-          .end();
-      })
-      .end();
-  });
 
-  it('Update the contents of a file', function(done) {
-    copyRecursiveSync('test/fixtures', '.tmp/fixtures_backup');
-    syncy(['test/fixtures/**'], '.tmp/update_content', { base: 'test/fixtures' })
-      .on('end', function() {
-        fs.writeFileSync('.tmp/fixtures_backup/folder-2/test.txt', 'test');
-        syncy(['.tmp/fixtures_backup/**'], '.tmp/update_content', { base: '.tmp/fixtures_backup' })
-          .on('end', function() {
-            var data = fs.readFileSync('.tmp/update_content/folder-2/test.txt', 'utf-8');
-            assert.equal(data, 'test');
-            done();
-          })
-          .end();
-      })
-      .end();
-  });
+    const streamOne = syncy(['.tmp/fixtures_backup_issue_1/**/*.txt'], '.tmp/issue_1');
+    const streamTwo = syncy(['.tmp/fixtures_backup_issue_1/**/*.txt'], '.tmp/issue_1');
 
-  it('No update and delete files from dest (updateAndDelete)', function(done) {
-    createFiles('.tmp/update_nodelete/test/fixtures', 3);
-    syncy(['test/fixtures/**'], '.tmp/update_nodelete', { updateAndDelete: false })
-      .on('end', function() {
-        var result = recurseReadDir('.tmp/update_nodelete');
-        // File `test-2.txt` overwritten
-        assert.equal(result.length, 10);
+    streamOne.on('end', () => {
+      // Remove one file in the source directory
+      fs.unlinkSync('.tmp/fixtures_backup_issue_1/folder-1/test.txt');
+
+      streamTwo.on('end', () => {
+        const result = recurseReadDir('.tmp/issue_1');
+        assert.equal(result.length, 7);
         done();
-      })
-      .end();
+      });
+
+      streamTwo.end();
+    });
+
+    streamOne.end();
+  });
+
+  it('Update the contents of a file', (done) => {
+    // Backup test files
+    copyRecursiveSync('test/fixtures', '.tmp/fixtures_backup');
+
+    const streamOne = syncy(['test/fixtures/**'], '.tmp/update_content', {
+      base: 'test/fixtures'
+    });
+
+    const streamtwo = syncy(['.tmp/fixtures_backup/**'], '.tmp/update_content', {
+      base: '.tmp/fixtures_backup'
+    });
+
+    streamOne.on('end', () => {
+      fs.writeFileSync('.tmp/fixtures_backup/folder-2/test.txt', 'test');
+
+      streamtwo.on('end', () => {
+        const data = fs.readFileSync('.tmp/update_content/folder-2/test.txt', 'utf-8');
+        assert.equal(data, 'test');
+        done();
+      });
+
+      streamtwo.end();
+    });
+
+    streamOne.end();
+  });
+
+  it('No update and delete files from dest (updateAndDelete)', (done) => {
+    createFiles('.tmp/update_nodelete/test/fixtures', 3);
+
+    const stream = syncy(['test/fixtures/**'], '.tmp/update_nodelete', {
+      updateAndDelete: false
+    });
+
+    stream.on('end', () => {
+      const result = recurseReadDir('.tmp/update_nodelete');
+      // File `test-2.txt` overwritten
+      assert.equal(result.length, 10);
+      done();
+    });
+
+    stream.end();
   });
 });
 
-describe('Console information', function() {
-  it('Verbose', function(done) {
+describe('Console information', () => {
+  it('Verbose', (done) => {
     // Hook for console output
-    var clgDump = console.log;
-    var stdout = '';
+    const clgDump = console.log;
+    let stdout = '';
     console.log = function() {
       stdout += JSON.stringify(arguments);
     };
 
-    syncy(['test/fixtures/**'], '.tmp/verbose', { verbose: true })
-      .on('end', function() {
-        assert.equal(/test\/fixtures\/test-2.txt/.test(stdout), true);
-        console.log = clgDump;
-        done();
-      })
-      .end();
+    const stream = syncy(['test/fixtures/**'], '.tmp/verbose', {
+      verbose: true
+    });
+
+    stream.on('end', () => {
+      console.log = clgDump;
+      assert.equal(/test\/fixtures\/test-2.txt/.test(stdout), true);
+      done();
+    });
+
+    stream.end();
   });
 });
 
-describe('Ignore files', function() {
-  it('Ignore `test-0.txt` in dest directory (ignoreInDest)', function(done) {
+describe('Ignore files', () => {
+  it('Ignore `test-0.txt` in dest directory (ignoreInDest)', (done) => {
     createFiles('.tmp/single_ignore/test/fixtures', 1);
-    syncy(['test/fixtures/**'], '.tmp/single_ignore', {
+    const stream = syncy(['test/fixtures/**'], '.tmp/single_ignore', {
       ignoreInDest: '**/test-0.txt'
-    })
-      .on('end', function() {
-        var result = recurseReadDir('.tmp/single_ignore');
-        assert.equal(result.length, 9);
-        done();
-      })
-      .end();
+    });
+
+    stream.on('end', () => {
+      const result = recurseReadDir('.tmp/single_ignore');
+      assert.equal(result.length, 9);
+      done();
+    });
+
+    stream.end();
   });
 });
